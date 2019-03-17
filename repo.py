edae09c3965a570316c4e032e8c538e0ecdcc4f0
karthik <karthikkannan@gmail.com>
2019-03-18 00:24:55 +0530

    Added repo pull command with colors.

--- repo.py
+++ repo.py
@@ -2,18 +2,50 @@
 import argparse
 import glob
 import os
+import subprocess
 
 from parse import parse
 
-
 REPO_HOME = os.environ.get('REPO_HOME', '~/code')
 
+TRED = '\033[31m'  # Red Text
+TGREEN = '\033[32m'  # Green Text
+TYELLOW = '\033[33m'  # Green Text
+ENDC = '\033[m'  # reset to the defaults
 
-def list_projects(args):
+
+def is_git_folder(folder):
+    return any(filter(lambda name: name.endswith('.git'), os.listdir(folder)))
+
+
+def ls_command(args):  # noqa
+    projects = list_projects()
+    print('\n'.join(projects))
+    return projects
+
+
+def list_projects():
     files = glob.glob(f'{REPO_HOME}/*/*/*')
-    projects = [file for file in files if os.path.isdir(file)]
+    projects = [file for file in files if os.path.isdir(file) and is_git_folder(file)]
+    return projects
+
+
+def update_projects(args):  # noqa
+    projects = list_projects()
     for project in projects:
-        print(project)
+        os.chdir(project)
+        print(TYELLOW + f' Updating project {project}: ' + ENDC)
+        result = subprocess.check_output('git branch', shell=True)
+        result = result.decode('unicode_escape').strip()
+        if result != '* master':
+            print(TRED + ' Not on Master. Skipping... ' + ENDC)
+            continue
+        result = subprocess.check_output('git diff-index --name-status HEAD', shell=True)
+        if result:
+            print(TRED + ' There are un-committed changes. Skipping... ' + ENDC)
+            continue
+        result = subprocess.check_output('git pull', shell=True)
+        print(TGREEN + result.decode('unicode_escape') + ENDC)
 
 
 def clone_project(args):
@@ -37,14 +69,17 @@ def parse_url(url):
 def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
-    parser.set_defaults(func=list_projects)
+    parser.set_defaults(func=ls_command)
 
     parser_clone = subparsers.add_parser('clone')
     parser_clone.add_argument('url', help='git url to clone', type=str)
     parser_clone.set_defaults(func=clone_project)
 
     parser_ls = subparsers.add_parser('ls')
-    parser_ls.set_defaults(func=list_projects)
+    parser_ls.set_defaults(func=ls_command)
+
+    parser_update = subparsers.add_parser('pull')
+    parser_update.set_defaults(func=update_projects)
 
     args = parser.parse_args()
     args.func(args)
